ad7ad129fcb532bd77b4a80f768351501c8df08f
Ziqi-Wang <450880548@qq.com>
2017-03-19 20:54:09 -0400

    wrote tcl

---
+++ src/test.tcl
@@ -0,0 +1,16 @@
+vlib work
+
+;# Compile components if any
+vcom five_stage_pipline.vhd
+vcom fetch.vhd
+vcom instruction_memory.vhd
+vcom id.vhd
+vcom exe.vhd
+vcom mem.vhd
+vcom wb.vhd
+
+;# Start simulation
+vsim five_stage_processor
+
+;# Run for 20,000 ns
+run 20000ns
